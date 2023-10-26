e99cd66855eb554fc3604baa230a304daf56ae56
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-10-26 11:43:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,66 @@ app.get("/books", async (rquest, response) => {
     }
 });
 
+
+
+app.get("/books/:id", async (request, response) => {
+
+    console.log(typeof request.params.id),
+
+    try {
+
+        const book = await prisma.books.findUnique({
+
+            where: {
+                id: Number(request.params.id),
+            },
+        });
+    
+    
+
+
+response.status(200).json(book);
+} catch (error) {
+    response.status(404).send({
+        message: "Midagi on katki",
+        error,
+    });
+}
+});
+
+
+
+
+
+
+
+
+
+app.delete("/books/:id", async (request, response) => {
+    try {
+        const deletedBook = await prisma.books.delete({
+            where: {
+                id: Number(request.params.id),
+            },
+        });
+
+        response.status(200).json(deletedBook);
+
+    } catch (error) {
+        response.status(404).send({
+            message: "Midagi on katki",
+            error,
+        });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log("Server listening at port ${PORT}");
 });
+
+
+
+
 
 
